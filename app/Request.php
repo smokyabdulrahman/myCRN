728f9bfb322cda5ea22c243d1a79942d5465e61f
smoky <a.alrahama@gmail.com>
2017-04-06 19:56:59 +0300

    phpmailer try

--- app/Request.php
+++ app/Request.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Mail;
+use PHPMailer;
 
 class Request extends Model
 {
@@ -46,12 +47,39 @@ class Request extends Model
 //
 //            });
 
-            $to      = 'a.alrahama@gmail.com';
-            $subject = 'the subject';
-            $message = 'hello';
-            $headers = 'X-Mailer: PHP/' . phpversion();
+            //Create a new PHPMailer instance
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+// change this to 0 if the site is going live
+            $mail->SMTPDebug = 2;
+            $mail->Debugoutput = 'html';
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = 587;
+            $mail->SMTPSecure = 'tls';
 
-            mail($to, $subject, $message, $headers);
+            //use SMTP authentication
+            $mail->SMTPAuth = true;
+//Username to use for SMTP authentication
+            $mail->Username = "a.alrahama@gmail.com";
+            $mail->Password = '$moky1417';
+            $mail->addAddress($request->email, 'Somebody');
+            $mail->Subject = 'Open!';
+            // $message is gotten from the form
+            $mail->msgHTML("<h1>$request</h1>
+<h2>hurry go register the course!!</h2>");
+            if (!$mail->send()) {
+                echo "We are extremely sorry to inform you that your message
+could not be delivered,please try again.";
+            } else {
+                echo "Your message was successfully delivered,you would be contacted shortly.";
+            }
+
+//            $to      = 'a.alrahama@gmail.com';
+//            $subject = 'the subject';
+//            $message = 'hello';
+//            $headers = 'X-Mailer: PHP/' . phpversion();
+//
+//            mail($to, $subject, $message, $headers);
 
         }
     }
